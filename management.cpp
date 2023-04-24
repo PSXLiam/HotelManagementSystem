7f69b1e882f8362d812e1bcde4766742b05cf523
Liam Rogers <LiamRogersUK@gmail.com>
2023-04-24 15:07:34 +0100

    Modify Cases with Functions, Intialise Prices All cases, excluding exit & default, are now defined as functions before the main. The price for each product is now initialised before the main. namespace std is no longer called, now only the names needed are called.

--- management.cpp
+++ management.cpp
@@ -1,19 +1,88 @@
 #include <iostream>
-using namespace std; 
+using std::cin, std::cout;
+
+int quant;
+int choice;
+int m=1;
+
+//Prices
+int roomPrice=1200, burgerPrice=120, drinkPrice=120;
+//Quantity
+int Qrooms=0, Qburger=0, Qdrinks=0;
+//Food Items sold
+int Srooms=0, Sburger=0, Sdrinks=0;
+//Total Price of Items
+int Total_rooms=0, Total_burger=0, Total_drinks=0;
+
+void Rooms()
+{
+  cout<<"\n\n Enter the Number of Rooms you want: ";
+  cin>>quant;
+  if(Qrooms-Srooms >= quant)
+    {
+      Srooms=Srooms+quant;
+      Total_rooms = Total_rooms+quant*roomPrice;
+      cout<<"\n\t"<<quant<<" Room/Rooms have been alloted to you.";
+    }
+  else
+    {
+      cout<<"\n\t Only "<<Qrooms-Srooms<<"Rooms Remaining in Hotel.";
+    }
+}
+
+void Burgers()
+{
+  cout<<"\n\n Enter Servings of Burger: ";
+  cin>>quant;
+  if(Qburger-Sburger >= quant)
+    {
+      Sburger=Sburger+quant;
+      Total_burger = Total_burger+quant*burgerPrice;
+      cout<<"\n\t"<<quant<<" Burgers in the order.";
+    }
+  else
+    {
+      cout<<"\n\t Only "<<Qburger-Sburger<<" Burgers Remaining in the Hotel.";
+    }
+}
+
+void Drinks()
+{
+  cout<<"\n\n Enter Servings of Drinks: ";
+  cin>>quant;
+  if(Qdrinks-Sdrinks >= quant)
+    {
+      Sdrinks=Sdrinks+quant;
+      Total_drinks = Total_drinks+quant*drinkPrice;
+      cout<<"\n\t"<<quant<<" Drinks in the order.";
+    }
+  else
+    {
+      cout<<"\n\t Only"<<Qdrinks-Sdrinks<<" Drinks Remaining in the Hotel.";
+    }
+}
+
+void Details()
+{
+  cout<<"\n Details of Sales and Collection: ";
+  cout<<"\n\t Number of Rooms we had: "<<Qrooms;
+  cout<<"\n\t Number of Rooms we gave for Rent: "<<Srooms;
+  cout<<"\n\t Remaining Rooms: "<<Qrooms-Srooms;
+  cout<<"\n\t Total Rooms Collection for the day: "<<Total_rooms;
+
+  cout<<"\n\n\t Number of Burgers we had: "<<Qburger;
+  cout<<"\n\t Number of Burgers Sold: "<<Sburger;
+  cout<<"\n\t Remaining Burgers: "<<Qburger-Sburger;
+  cout<<"\n\t Total Burger Collection for the day: "<<Total_burger;
+
+  cout<<"\n\n\t Number of Drinks we had: "<<Qdrinks;
+  cout<<"\n\t Number of Drinks sold: "<<Sdrinks;
+  cout<<"\n\t Remaining Drinks: "<<Qdrinks-Sdrinks;
+  cout<<"\n\t Total Drink Collection for the day: "<<Total_drinks;  
+}
 
 int main()
 {
-    int quant;
-    int choice;
-    int m=1;
-
-    //Quantity
-    int Qrooms=0, Qburger=0, Qdrinks=0;
-    //Food Items sold
-    int Srooms=0, Sburger=0, Sdrinks=0;
-    //Total Price of Items
-    int Total_rooms=0, Total_burger=0, Total_drinks=0;
-
     cout<<"\n Quantity of Items in Hotel";
     cout<<"\n Rooms Available: ";
     cin>>Qrooms;
@@ -37,69 +106,19 @@ int main()
       switch(choice)
       {
         case 1:
-          cout<<"\n\n Enter the Number of Rooms you want: ";
-          cin>>quant;
-          if(Qrooms-Srooms >= quant)
-          {
-              Srooms=Srooms+quant;
-              Total_rooms = Total_rooms+quant*1200;
-              cout<<"\n\t"<<quant<<" Room/Rooms have been alloted to you.";
-              break;
-          }
-          else
-          {
-              cout<<"\n\t Only "<<Qrooms-Srooms<<"Rooms Remaining in Hotel.";
-              break;
-          }
+          Rooms();
+          break;
 
         case 2:
-          cout<<"\n\n Enter Servings of Burger: ";
-          cin>>quant;
-          if(Qburger-Sburger >= quant)
-          {
-              Sburger=Sburger+quant;
-              Total_burger = Total_burger+quant*120;
-              cout<<"\n\t"<<quant<<" Burgers in the order.";
-              break;
-          }
-          else
-          {
-              cout<<"\n\t Only "<<Qburger-Sburger<<" Burgers Remaining in the Hotel.";
-              break;
-          }
+          Burgers();
+          break;
 
         case 3:
-          cout<<"\n\n Enter Servings of Drinks: ";
-          cin>>quant;
-          if(Qdrinks-Sdrinks >= quant)
-          {
-              Sdrinks=Sdrinks+quant;
-              Total_drinks = Total_drinks+quant*120;
-              cout<<"\n\t"<<quant<<" Drinks in the order.";
-              break;
-          }
-          else
-          {
-              cout<<"\n\t Only"<<Qdrinks-Sdrinks<<" Drinks Remaining in the Hotel.";
-              break;
-          }
+          Drinks();
+          break;
 
         case 4:
-          cout<<"\n Details of Sales and Collection: ";
-          cout<<"\n\t Number of Rooms we had: "<<Qrooms;
-          cout<<"\n\t Number of Rooms we gave for Rent: "<<Srooms;
-          cout<<"\n\t Remaining Rooms: "<<Qrooms-Srooms;
-          cout<<"\n\t Total Rooms Collection for the day: "<<Total_rooms;
-
-          cout<<"\n\n\t Number of Burgers we had: "<<Qburger;
-          cout<<"\n\t Number of Burgers Sold: "<<Sburger;
-          cout<<"\n\t Remaining Burgers: "<<Qburger-Sburger;
-          cout<<"\n\t Total Burger Collection for the day: "<<Total_burger;
-
-          cout<<"\n\n\t Number of Drinks we had: "<<Qdrinks;
-          cout<<"\n\t Number of Drinks sold: "<<Sdrinks;
-          cout<<"\n\t Remaining Drinks: "<<Qdrinks-Sdrinks;
-          cout<<"\n\t Total Drink Collection for the day: "<<Total_drinks;
+          Details();
           break;
 
         case 5:
